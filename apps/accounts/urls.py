47a977cbc7dd2d8be8290d1bebd0cf18d33aab23
Salah Saleh <salahaldbyby00@gmail.com>
2026-03-19 21:32:54 +0300

    last Devlopment

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path('forgot-password/', views.ForgotPasswordView.as_view(), name='forgot-password'),
     path('verify-reset-otp/', views.VerifyResetOTPView.as_view(), name='verify-reset-otp'),
     path('reset-password/', views.ResetPasswordView.as_view(), name='reset-password'),
+    path('google/', views.GoogleLoginView.as_view(), name='google-login'),
 ]
